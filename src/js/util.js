c8b5d6a8db2edba9f111eec4c7ae0028b6da6dd0
Guilherme <bsbtrindade@gmail.com>
2021-03-05 00:07:23 -0300

    feat: add pushConciliacao

---
+++ src/js/util.js
@@ -0,0 +1,16 @@
+export const nameMonthToMonth = (name) => {
+    switch (name) {
+        case 'JAN': return 1
+        case 'FEV': return 2
+        case 'MAR': return 3
+        case 'ABR': return 4
+        case 'MAI': return 5
+        case 'JUN': return 6
+        case 'JUL': return 7
+        case 'AGO': return 8
+        case 'SET': return 9
+        case 'OUT': return 10
+        case 'NOV': return 11
+        case 'DEZ': return 12
+    }
+}
--- src/views/Conciliacao/Conciliacao.vue
+++ src/views/Conciliacao/Conciliacao.vue
@@ -5,7 +5,17 @@
 				<v-textarea rows="1" label="Cole o extrato do nubank" no-resize v-model="textNubank"></v-textarea>
 				<v-btn @click="loadNubank" color="success">Carregar</v-btn>
 				<div>
-					<v-card v-for="conta in dadosNubank" :key="conta" class="mt-4">
+					<v-card
+						@click="clickSyncNu(index)"
+						:disabled="conta.conciliado"
+						v-for="(conta, index) in dadosNubank"
+						:key="conta.data+conta.descricao+conta.valor"
+						class="mt-4"
+						:class="[
+							index == indexMobillsSync ? 'selected' : '',
+							conta.conciliado ? 'conciliado' : ''
+						]"
+					>
 						<div class="d-flex flex-column px-2">
 							<div class="d-flex justify-space-between">
 								<span class="small">{{conta.data}}</span>
@@ -30,7 +40,17 @@
 				<v-file-input ref="inputFile" label="Selecionar arquivo .xls"></v-file-input>
 				<v-btn @click="loadMobills" color="success">Carregar</v-btn>
 				<div>
-					<v-card v-for="conta in dadosMobills" :key="conta" class="mt-4">
+					<v-card
+						@click="clickSyncMobills(index)"
+						:disabled="conta.conciliado"
+						v-for="(conta,index) in dadosMobills"
+						:key="conta.data + conta.descricao + conta.valor"
+						class="mt-4"
+						:class="[
+							index == indexMobillsSync ? 'selected' : '',
+							conta.conciliado ? 'conciliado' : ''
+						]"
+					>
 						<div class="d-flex flex-column px-2">
 							<div class="d-flex justify-space-between">
 								<span class="small">{{conta.data}}</span>
@@ -49,6 +69,8 @@
 
 <script>
 import readXlsxFile from "read-excel-file";
+import { nameMonthToMonth } from "../../js/util";
+import moment from "moment";
 
 export default {
 	components: {},
@@ -58,19 +80,29 @@ export default {
 			dadosNubank: [],
 			xlsMobills: [],
 			dadosMobills: [],
+			indexNuSync: undefined,
+			indexMobillsSync: undefined,
+			dadosSincronismo: [],
 		};
 	},
 	computed: {},
+
 	methods: {
 		loadNubank() {
 			let dados = this.textNubank.match(/\d{2} \D{3}\n.*/g);
 
 			this.dadosNubank = dados.map((value) => {
 				value = value.split(/\n/g);
+				let data = value[0];
+				let [dia, mes] = data.split(" ");
+				dia = dia.toString().padStart(2, "0");
+				mes = nameMonthToMonth(mes).toString().padStart(2, "0");
+
 				return {
-					data: value[0],
+					data: moment(`2021-${mes}-${dia}`).format("DD/MM/YYYY"),
 					descricao: value[1].match(/.*	/g)[0].trim(),
 					valor: value[1].match(/	.*/g)[0].trim(),
+					conciliado: 0,
 				};
 			});
 		},
@@ -78,13 +110,37 @@ export default {
 			this.xlsMobills = this.xlsMobills[0];
 			this.xlsMobills.splice(0, 1);
 			this.dadosMobills = this.xlsMobills.map((value) => {
+				let [dia, mes, ano] = value[0].split("/");
 				return {
+					moment: moment([ano, mes - 1, dia]),
 					data: value[0],
 					descricao: value[1],
 					valor: value[3].replace("R$ ", ""),
+					conciliado: 0,
 				};
 			});
-			console.log(this.dadosMobills);
+
+			this.dadosMobills = this.dadosMobills.reverse();
+		},
+		clickSyncNu(index) {
+			this.indexNuSync = index;
+			this.pushConciliacao();
+		},
+		clickSyncMobills(index) {
+			this.indexMobillsSync = index;
+			this.pushConciliacao();
+		},
+		pushConciliacao() {
+			if (this.indexNuSync != undefined && this.indexMobillsSync != undefined) {
+				this.dadosNubank[this.indexNuSync].conciliado = 1;
+				this.dadosMobills[this.indexMobillsSync].conciliado = 1;
+				this.dadosSincronismo.push({
+					nubank: this.dadosNubank[this.indexNuSync],
+					mobills: this.dadosMobills[this.indexMobillsSync],
+				});
+				this.indexNuSync = undefined;
+				this.indexMobillsSync = undefined;
+			}
 		},
 	},
 	async mounted() {
@@ -108,4 +164,10 @@ export default {
 
 .conciliacao span.small
 	font-size: 12px
+
+.conciliacao .selected
+	border: 1px solid green
+
+.conciliacao .conciliado
+	color: green
 </style>
